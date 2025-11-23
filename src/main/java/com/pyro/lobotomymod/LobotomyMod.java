21300b469ab57f31121735e6004947b4a7e3bb75
Sebastian <sebastian.does.stuff64628@gmail.com>
2025-11-23 11:55:06 -0600

    Nothing added just some configuration

---
+++ src/main/java/com/pyro/lobotomymod/LobotomyMod.java
@@ -0,0 +1,16 @@
+package com.pyro.lobotomymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LobotomyMod implements ModInitializer {
+	public static final String MOD_ID = "lobotomymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/pyro/lobotomymod/LobotomyModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pyro.lobotomymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LobotomyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
