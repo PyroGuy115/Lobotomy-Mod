6effa07d2591f15bd35ff1d2b0b6f4cd125bcf5e
Sebastian <sebastian.does.stuff64628@gmail.com>
2025-11-24 02:27:44 -0600

    Small updates

--- src/main/java/com/pyro/lobotomymod/block/ModBlocks.java
+++ src/main/java/com/pyro/lobotomymod/block/ModBlocks.java
@@ -1,22 +1,14 @@
 package com.pyro.lobotomymod.block;
 
 import com.pyro.lobotomymod.LobotomyMod;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block BLOCK_OF_HITLER = registerBlock("block_of_hitler",
-            new Block(AbstractBlock.Settings.create().strength(4f)
-                    .requiresTool().sounds(BlockSoundGroup.STONE)));
-
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, Identifier.of(LobotomyMod.MOD_ID, name), block);
@@ -30,8 +22,5 @@ public class ModBlocks {
     public static void registerModBlocks() {
         LobotomyMod.LOGGER.info("Registering your shity blocks for " + LobotomyMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-            entries.add(ModBlocks.BLOCK_OF_HITLER);
-        });
     }
 }
